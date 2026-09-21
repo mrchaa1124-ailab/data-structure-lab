e682358e87bc27af0b3a4390466a1dbd78f6353b
chacha <mrchaa1124@gmail.com>
2026-09-21 13:13:20 +0900

    lec03 pointer

---
+++ Lec03_Array_Class/Array/array_pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // printf 사용을 위한 헤더 (또는 <cstdio> 가능)
+
+// 포인터를 이용한 두 변수의 값 교체 
+void swap(int* px, int* py) {
+    int tmp;
+    tmp = *px;
+    *px = *py;
+    *py = tmp;
+}
+
+// C++ 표준에 맞게 int main()으로 수정
+int main() {
+    int a = 1, b = 2;
+
+    printf("SWAP을 호출하기 전: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+
+    // 출력 메시지의 오타를 '호출한 후'로 수정하고 결과를 확인합니다.
+    printf("SWAP을 호출한 후: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
